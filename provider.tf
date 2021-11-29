e29504d039d30554096cf40ecc794da9b91152e2
savitapal8 <78473714+savitapal8@users.noreply.github.com>
2021-11-29 16:18:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,8 @@
 provider "google" {
+  user_project_override = true
   access_token          = var.access_token
+  project               = "airline1-sabre-wolverine"
+  region                = "us-central1"
 }
 
 provider "google-beta"{
